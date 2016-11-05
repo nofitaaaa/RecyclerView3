f0971aa7a9caa7a0264596aa706820b0c3e1447d
nofita <nofitamahfudiyah@gmail.com>
2016-11-05 10:18:06 +0700

    RecyclerView List Card

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
@@ -1,15 +1,32 @@
 package id.sch.smktelkom_mlg.learn.recyclerview3;
 
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+import id.sch.smktelkom_mlg.learn.recyclerview3.adapter.HotelAdapter;
+import id.sch.smktelkom_mlg.learn.recyclerview3.model.Hotel;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    ArrayList<Hotel> mList = new ArrayList<>();
+    HotelAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +43,17 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        mAdapter = new HotelAdapter(mList);
+        recyclerView.setAdapter(mAdapter);
+
+        fillData();
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -48,5 +75,32 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void fillData()
+    {
+        Resources resources = getResources();
+
+        String [] arJudul = resources.getStringArray(R.array.places);
+        String [] arDeskripsi = resources.getStringArray(R.array.place_desc);
+        TypedArray a =resources.obtainTypedArray(R.array.places_picture);
+        Drawable [] arfoto = new Drawable[a.length()];
+
+        for (int i = 0; i < arfoto.length; i++)
+        {
+            BitmapDrawable bd = (BitmapDrawable) a.getDrawable(i);
+            RoundedBitmapDrawable rbd = RoundedBitmapDrawableFactory.create(getResources(),bd.getBitmap());
+            rbd.setCircular(true);
+            arfoto[i] = rbd;
+        }
+
+        a.recycle();
+
+        for (int i = 0; i < arJudul.length; i++)
+        {
+            mList.add( new Hotel (arJudul[i],arDeskripsi[i],arfoto[i]));
+        }
+
+        mAdapter.notifyDataSetChanged();
     }
 }
